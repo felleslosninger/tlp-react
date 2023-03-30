decb6e90ab499c78aaa0c74631f1388e3f2bdbc0
Øyvind Thune <oyvlei@gmail.com>
2023-03-30 08:37:24 +0200

    chore: initialize project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  moduleNameMapper: {
+    '.(css|less|scss)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/test/__mocks__/svg.ts',
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+  },
+};
