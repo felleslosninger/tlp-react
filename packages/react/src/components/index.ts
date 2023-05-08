bc29bdb97546b6b68ef2c23105c3cba558612ff8
Øyvind Thune <oyvlei@gmail.com>
2023-05-08 13:54:32 +0200

    fix: added Link and Container to export

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,4 +1,8 @@
 export { Header } from './Header';
 export type { HeaderProps } from './Header';
+export { Link } from './Link';
+export type { LinkProps } from './Link';
 export { LinkList } from './LinkList';
 export type { LinkListProps } from './LinkList';
+export { Container } from './Container';
+export type { ContainerProps } from './Container';
