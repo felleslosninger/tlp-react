d745ec81e9f8278af0933ea8461aa6cb81cd60eb
Jens Mo <jens.mo@digdir.no>
2023-06-01 13:57:14 +0200

    Changes children props in footer, added a wrapper around containers

--- packages/react/src/components/Footer/Footer.tsx
+++ packages/react/src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import classes from './Footer.module.css';
 
 interface FooterProps {
   children:
-    | React.ReactElement<FooterColumnProps>
+    | Array<React.ReactElement<FooterColumnProps>>
     | React.ReactElement<FooterBottomProps>;
 }
 
@@ -21,20 +21,22 @@ interface FooterBottomProps {
 const Footer = ({ children }: FooterProps) => {
   return (
     <footer className={classes.footer}>
-      <Container className={classes.topContainer}>
-        <div className={classes.topContent}>
-          {React.Children.map(children, (child) => (
-            <>{child && child.type === Footer.Column ? child : ''}</>
-          ))}
-        </div>
-      </Container>
-      <Container className={classes.bottomContainer}>
-        <div className={classes.bottomContent}>
-          {React.Children.map(children, (child) => (
-            <>{child.type === Footer.Bottom ? child : ''}</>
-          ))}
-        </div>
-      </Container>
+      <div className={classes.containerWrapper}>
+        <Container className={classes.topContainer}>
+          <div className={classes.topContent}>
+            {React.Children.map(children, (child) => (
+              <>{child && child.type === Footer.Column ? child : ''}</>
+            ))}
+          </div>
+        </Container>
+        <Container className={classes.bottomContainer}>
+          <div className={classes.bottomContent}>
+            {React.Children.map(children, (child) => (
+              <>{child.type === Footer.Bottom ? child : ''}</>
+            ))}
+          </div>
+        </Container>
+      </div>
     </footer>
   );
 };
