d4707b8ab55f859cf2460c5746d0d52e5d038c56
Jens Mo <97087448+jensmo98@users.noreply.github.com>
2023-06-22 14:02:44 +0200

    More stories for header

--- packages/react/src/components/Header/Header.stories.tsx
+++ packages/react/src/components/Header/Header.stories.tsx
@@ -6,7 +6,7 @@ import { Link } from '../Link/Link';
 import { Header } from './Header';
 import classes from './HeaderStories.module.css';
 
-const children = [
+const AllChildren = [
   <Header.Left key={0}>
     <a href='.'>
       <img
@@ -29,12 +29,53 @@ const children = [
         </li>
       </ul>
     </nav>
-    <p className={classes.middleLi}>Middle</p>
-    <p>Middle</p>
   </Header.Middle>,
   <Header.Right key={2}>Right</Header.Right>,
   <Header.Bottom key={3}>
-    <p>Header bottom</p>
+    <nav className={classes.bottomNav}>
+      <div className={classes.bottomDiv}>
+        <h2>Links</h2>
+        <ul className={classes.mobileUl}>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Link to something</Link>
+          </li>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Link to something</Link>
+          </li>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Link to something</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={classes.bottomDiv}>
+        <h2>Languange</h2>
+        <ul className={classes.mobileUl}>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Norsk</Link>
+          </li>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Engelsk</Link>
+          </li>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Samisk</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={classes.bottomDiv}>
+        <h2>Other</h2>
+        <ul className={classes.mobileUl}>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Link to something</Link>
+          </li>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Link to something</Link>
+          </li>
+          <li className={classes.bottomLi}>
+            <Link href='#'>Link to something</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </Header.Bottom>,
   <Header.Mobile key={4}>
     <nav className={classes.mobileNav}>
@@ -78,6 +119,37 @@ const children = [
     </nav>
   </Header.Mobile>,
 ];
+const WithoutBottomChildren = [
+  <Header.Left key={0}>
+    <a href='.'>
+      <img
+        src={myLogo}
+        alt='SVG logo'
+      />
+    </a>
+  </Header.Left>,
+  <Header.Middle key={1}>
+    <nav>
+      <ul className={classes.middleUl}>
+        <li className={classes.middleLi}>
+          <Link href='#'>Link to something</Link>
+        </li>
+        <li className={classes.middleLi}>
+          <Link href='#'>Link to something</Link>
+        </li>
+        <li className={classes.middleLi}>
+          <Link href='#'>Link to something</Link>
+        </li>
+      </ul>
+    </nav>
+  </Header.Middle>,
+  <Header.Right key={2}>Right</Header.Right>,
+  <Header.Mobile key={4}>
+    <nav className={classes.mobileNav}>
+      <h2 className={classes.mobileH2}>Mobile Menu</h2>
+    </nav>
+  </Header.Mobile>,
+];
 
 export default {
   title: 'Components/Header',
@@ -93,7 +165,12 @@ export default {
 
 export const Primary = {
   args: {
-    backgroundColor: 'red',
-    children: children,
+    children: AllChildren,
+  },
+};
+
+export const WithoutBottom = {
+  args: {
+    children: WithoutBottomChildren,
   },
 };
