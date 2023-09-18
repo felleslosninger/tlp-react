871f61ea87b60cf24c301d336ed8c5bca6798909
Tobias Barsnes <tobias.barsnes@digdir.no>
2023-09-18 16:02:52 +0200

    add tests to FileCard

--- packages/react/src/components/FileCard/FileCard.test.tsx
+++ packages/react/src/components/FileCard/FileCard.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render as renderRtl, screen } from '@testing-library/react';
+import { FileJsonIcon } from '@navikt/aksel-icons';
 
 import type { FileCardProps } from './FileCard';
 import { FileCard } from './FileCard';
@@ -14,6 +15,40 @@ describe('FileCard', () => {
     });
     const list = screen.getByLabelText('test');
     expect(list.classList).toContain('fileCard');
+  });
+
+  it('should render the heading', () => {
+    render({
+      heading: 'test heading',
+      subHeading: 'test',
+      filePath: '/test',
+      ariaLabel: 'test',
+    });
+    const heading = screen.getByText('test heading');
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should render the subHeading', () => {
+    render({
+      heading: 'test',
+      subHeading: 'test subHeading',
+      filePath: '/test',
+      ariaLabel: 'test',
+    });
+    const subHeading = screen.getByText('test subHeading');
+    expect(subHeading).toBeInTheDocument();
+  });
+
+  it('should render the file icon when icon is passed', () => {
+    render({
+      heading: 'test',
+      subHeading: 'test',
+      filePath: '/test',
+      ariaLabel: 'test',
+      icon: <FileJsonIcon title='JSON Icon' />,
+    });
+    const fileIcon = screen.getByTitle('JSON Icon');
+    expect(fileIcon).toBeInTheDocument();
   });
 });
 
